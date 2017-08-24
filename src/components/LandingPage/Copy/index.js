37625ecca9cb0b9fd03042bb2555721d02cd43ff
Riki Konikoff <riki.konikoff@gmail.com>
2017-08-24 13:50:43 -0400

    refacrtor landing page copy

--- src/components/LandingPage/Copy/index.js
+++ src/components/LandingPage/Copy/index.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleLogin from '../GoogleLogin';
 
-class LandingPageCopy extends Component {
-  render(){
-    return(
-      <div>
-        <div className='welcome'>
-          Welcome to Caravan
-          <div className='welcome-description'>
-            Taking a road trip? Organize how
-            travelers will get to their destination.
-          </div>
-        </div>
-        <GoogleLogin />
+const LandingPageCopy = () => (
+  <div>
+    <div className='welcome'>
+      Welcome to Caravan
+      <div className='welcome-description'>
+        Taking a road trip? Organize how
+        travelers will get to their destination.
       </div>
-    )
-  }
-}
+    </div>
+    <GoogleLogin />
+  </div>
+)
 
 export default LandingPageCopy;
