c975ef2692c5f0df0c964cc359409f7c2cf6f7ca
Riki Konikoff <riki.konikoff@gmail.com>
2017-08-24 13:51:05 -0400

    refactor horizontal loop car

--- src/components/LandingPage/HorizontalLoopCar/index.js
+++ src/components/LandingPage/HorizontalLoopCar/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { carImage } from '../../../utilities/imageUrls'
 
-const HorizontalLoopCar = (props) => {
-  return(
-    <div className='car-wrapper'>
-      <img className='car' src={carImage} alt='' />
-    </div>
-  )
-}
+const HorizontalLoopCar = () => (
+  <div className='car-wrapper'>
+    <img className='car' src={carImage} alt='' />
+  </div>
+)
 
 export default HorizontalLoopCar;
